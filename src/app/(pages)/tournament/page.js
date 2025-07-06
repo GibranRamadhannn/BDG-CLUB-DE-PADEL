71e4089ef29e22fe534c27bbd91ca4d210033073
M Gibran Ramadhan <144303284+GibranRamadhannn@users.noreply.github.com>
2025-07-06 10:15:39 +0700

    Disable register

--- src/app/(pages)/tournament/page.js
+++ src/app/(pages)/tournament/page.js
@@ -64,7 +64,7 @@ export default function TournamentPage() {
             <TournamentCard
               key={tournament.id}
               tournament={tournament}
-              showRegisterButton={true}
+              showRegisterButton={false}
             />
           ))}
       </div>
